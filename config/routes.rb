1cdf3c61b5ac60d41441b9698b5e650734802ee4
antonin danto <antonindanto@gmail.com>
2024-02-20 13:07:49 +0100

    RAILS TASK MANAGER

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,21 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get "tasks", to: "tasks#index"
+
+  # delete "tasks/:id", to: "tasks#delete"
+
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  # patch "tasks/:id", to: "tasks#update", as: :update_task
+
+  # get "tasks/new", to: "tasks#new", as: :new_task
+
+  # post "tasks", to: "tasks#create", as: :create_task
+
+  # get "tasks/:id", to: "tasks#show", as: :task
+
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :tasks
 end
